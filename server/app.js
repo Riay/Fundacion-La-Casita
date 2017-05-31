e1fbf7fe5bd6765c7fb392296d999ba5067508e2
Yair Silva Mendoza <yairs.mendoza@hotmail.com>
2017-05-31 15:21:21 +0200

    First Commit

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = 3000
+
+const app = express()
+
+
+app.use(express.static( path.join(__dirname, '../client') ))
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/Acerca', (req, res) => {
+  res.render('About')
+})
+
+app.get('/Historia', (req, res) => {
+  res.render('Historia')
+})
+
+app.get('/Cursos', (req, res) => {
+  res.render('Courses')
+})
+
+
+app.listen(PORT)
+console.log(`Listening on PORT ${PORT}`);
